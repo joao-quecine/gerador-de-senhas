5112c42d37023d647fe9e01d61b9bf96a548a3ba
Joao Pedro quecine Grande <joaoquecinepessoal@gmail.com>
2022-08-12 16:00:03 -0300

    novas opçoes de senhas adicionadas

--- gerador.py
+++ gerador.py
@@ -1,7 +1,7 @@
 
 from ast import Try
+from msilib.schema import ComboBox
 from random import choice
-from colorama import Fore
 from PySimpleGUI import PySimpleGUI as sg
 senha=[]
 
@@ -9,10 +9,9 @@ senha=[]
 sg.theme('Black')
 layout=[
     [sg.Text('digite o numero de caracteres sua senha deve ter'), sg.Input(key='caracteres', size=(2,0))],
-    [sg.Checkbox('senha forte',key='forte')],
+    [sg.Check('letras maiusculas', key='lm'),sg.Check('numeros', key='nm'), sg.Check('simbulos', key='sm')],
     [sg.Text('sua senha: ', key='xx')],
     [sg.Button('gerar')],
-
 ]
 
 #janela
@@ -24,8 +23,36 @@ while True:
 
     if eventos == 'gerar':
         try:
-            for i in range(int(valores['caracteres'])):
-             senha.append(choice('abcdefjhilmnopqrstuvwxyv1234567890'))
+            if valores['lm']==True and valores['nm']==True and valores['sm']==True:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefjhilmnopqrstuvwxyvz1234567890ABCDEFGHIJKLMNWXYZOPQRSTUV@#*&%$!_-{.}'))
+              
+            elif valores['lm']==True and valores['nm']==False and valores['sm']==False:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefghijwxyvklmnopqrstuvABCDEFGHIJKLMNOPQRSTUVWXYV'))
+            
+            elif valores['lm']==True and valores['nm']==True and valores['sm']==False:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefghijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890'))
+            
+            elif valores['lm']==True and valores['sm']==True and valores['nm']==False:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#*&%$!_-{.}'))
+            
+            elif valores['nm']==True and valores['lm']==False and valores['sm']==False:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefghijklmnopqrstuvwxyz1234567890'))
+            
+            elif valores['nm']==True and valores['lm']==False and valores['sm']==True:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefghijklmnopqrstuvwxyz1234567890@#*&%$!_-{.}'))
+
+            elif valores['sm']==True and valores['lm']==False and valores['nm']==False:
+                for i in range(int(valores['caracteres'])):
+                    senha.append(choice('abcdefghijklmnopqrstuvwxyz@#*&%$!_-{.}'))
+                
+                         
+            
             senha=''.join(senha)
             janela['xx'].update(f'sua senha: {senha}')
             senha=[]
