0fdf476706560dc859bc49c73ceb1a9375ed6d20
Joao Pedro quecine Grande <joaoquecinepessoal@gmail.com>
2022-08-11 21:04:25 -0300

    nova variedade de caracteres adicionado

--- gerador.py
+++ gerador.py
@@ -21,7 +21,7 @@ while True:
         break
     if eventos == 'gerar':
         for i in range(int(valores['caracteres'])):
-            senha.append(choice('adf'))
+            senha.append(choice('abcdefjhilmnopqrstuvwxyv1234567890'))
     senha=''.join(senha)
     janela['xx'].update(f'sua senha: {senha}')
     
