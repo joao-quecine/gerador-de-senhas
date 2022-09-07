ad446a21102049ac5b4da5bf1cb30cd94336e7ea
Joao Pedro Quecine Grande <quecinejoaopedro@gmail.com>
2022-09-07 20:10:04 -0300

    atualizaçao final da versao 1.0, com melhoria de codigo e adiçao de uma opçao a mais

--- gerador.py
+++ gerador.py
@@ -3,18 +3,25 @@ from ast import Try
 from msilib.schema import ComboBox
 from random import choice
 from PySimpleGUI import PySimpleGUI as sg
-senha=[]
 
-#layout
+
+
+#   declarando as variaveis
+senha=[]
+senha_final=[]
+
+
+#   layout da janela
 sg.theme('Black')
 layout=[
     [sg.Text('digite o numero de caracteres que sua senha deve ter'), sg.Input(key='caracteres', size=(2,0))],
-    [sg.Check('letras maiusculas', key='lm'),sg.Check('numeros', key='nm'), sg.Check('simbolos', key='sm')],
+    [sg.Check('letras', key='l'),sg.Check('letras maiusculas', key='lm'),sg.Check('numeros', key='nm'), sg.Check('simbolos', key='sm')],
     [sg.Text('sua senha: ', key='xx')],
     [sg.Button('gerar')],
 ]
 
-#janela
+
+#   criando sistema de atualizaçao de tela
 janela=sg.Window('gerador de senha',layout, element_justification='c')
 while True:
     eventos, valores= janela.read(timeout=6)
@@ -23,47 +30,36 @@ while True:
 
     if eventos == 'gerar':
         try:
-            if valores['lm']==True and valores['nm']==True and valores['sm']==True:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefjhilmnopqrstuvwxyvz1234567890ABCDEFGHIJKLMNWXYZOPQRSTUV@#*&%$!_-{.}'))
-              
-            elif valores['lm']==True and valores['nm']==False and valores['sm']==False:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijwxyvklmnopqrstuvABCDEFGHIJKLMNOPQRSTUVWXYV'))
-            
-            elif valores['lm']==True and valores['nm']==True and valores['sm']==False:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890'))
-            
-            elif valores['lm']==True and valores['sm']==True and valores['nm']==False:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#*&%$!_-{.}'))
-            
-            elif valores['nm']==True and valores['lm']==False and valores['sm']==False:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijklmnopqrstuvwxyz1234567890'))
-            
-            elif valores['nm']==True and valores['lm']==False and valores['sm']==True:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijklmnopqrstuvwxyz1234567890@#*&%$!_-{.}'))
+            if  valores['lm']== True:
+                senha.append('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+            if valores['nm']==True:
+                senha.append('12345678901234567890')
+            if valores['sm']==True:
+                senha.append('@#$%&*"()^?/_-@#$%&*"()^?/_-')
+            if valores['l']==True:
+                senha.append('abcdefghijklmnopqrstuvwxyz')
 
-            elif valores['sm']==True and valores['lm']==False and valores['nm']==False:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijklmnopqrstuvwxyz@#*&%$!_-{.}'))
-
-            elif valores['sm']==False and valores['lm']==False and valores['nm']==False:
-                for i in range(int(valores['caracteres'])):
-                    senha.append(choice('abcdefghijklmnopqrstuvwxyz'))
-
-                
-                         
             
             senha=''.join(senha)
-            janela['xx'].update(f'sua senha: {senha}')
+            senha=[x for x in senha]
+            
+            for i in range(int(valores['caracteres'])):
+                senha_final.append(choice(senha))
+            
+            senha_final=''.join(senha_final)
+            janela['xx'].update(f'sua senha: {senha_final}')
             senha=[]
+            senha_final=[]
 
-        except ValueError:
+
+        #  tratando erros
+        except ValueError or IndexError:
             janela['xx'].update('insira um valor numerico entre 0 a 99')
+
+
+
+            
+    
 
             
             
